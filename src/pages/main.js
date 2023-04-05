9696edf6c1633d5bf0c418e2d71ccde607ef4241
theghostyousee <patrickserise174@gmail.com>
2023-04-05 17:08:29 +0200

    s

--- src/pages/main.js
+++ src/pages/main.js
@@ -3,7 +3,7 @@ import React from "react";
 import Butterfly from "./media/butterfly.svg";
 function main() {
   const tweetLink =
-    "https://twitter.com/intent/tweet?text=Upgrade%20your%20investments%20with%20%40FortressDefi%0D%0A%0D%0AThe%20next-level%20decentralized%20investment%20platform%20on%20%40Arbitrum";
+    "https://twitter.com/intent/tweet?text=This%20is%20my%20proof%20of%20participation%20for%20%40winderxyz%F0%9F%AB%A1%0D%0A%0D%0AA%20new%20solidly%20fork%20ve%283%2C3%29%20coming%20on%20%23zkSync.%20&";
 
   const handleClick = () => {
     window.open(tweetLink, "_blank");
@@ -16,15 +16,11 @@ function main() {
           <img src={Butterfly}></img>
         </div>
         <div className="title">
-          <h2>FORTRESS PROTOCOL </h2>
+          <h2>Winder <br /> 
+             Upcoming ve(3,3) DEX on @zkSync </h2>
         </div>
 
-        <div className="paragraph">
-          <p>
-            Experience a next-level decentralized investment platform on
-            Arbitrum
-          </p>
-        </div>
+
         <div className="btn">
           <button onClick={handleClick}>Apply now..</button>
         </div>
